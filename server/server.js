5b937548cc216c2621b66249b5581ade771eeb6c
Gourav-raj <gouravrj9654@gmail.com>
2020-04-03 00:23:24 +0530

    without css

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://gourav:rg123rg1@lyricaldb-jbt9k.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
